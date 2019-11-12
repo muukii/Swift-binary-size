257fd0091186e3fa2178870be03525e3ece1c25d
Muukii <muukii.app@gmail.com>
2019-11-12 23:43:15 +0900

    Initial Commit

---
+++ BinarySize/BinarySize.h
@@ -0,0 +1,19 @@
+//
+//  BinarySize.h
+//  BinarySize
+//
+//  Created by muukii on 2019/11/12.
+//  Copyright © 2019 eureka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinarySize.
+FOUNDATION_EXPORT double BinarySizeVersionNumber;
+
+//! Project version string for BinarySize.
+FOUNDATION_EXPORT const unsigned char BinarySizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinarySize/PublicHeader.h>
+
+
